ebb7fa73a1bee6d24b517b68b94a2e170b1e760b
alexandr <safip@mail.ru>
2021-03-07 10:02:45 -1200

    add: change routing

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module example
 go 1.13
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.7.3
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/mattn/go-sqlite3 v2.0.2+incompatible
-	golang.org/x/crypto v0.0.0-20200115085410-6d4e4cb37c7d // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
+
+//IndexHandler fubction
+func IndexHandler(entrypoint string) func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, entrypoint)
+	}
+	return http.HandlerFunc(fn)
+}
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	//	staticFiles := http.FileServer(http.Dir("frontend/dist/static"))
+
+	//	r.PathPrefix("/css").Handler(staticFiles)
+	//	r.PathPrefix("/js").Handler(staticFiles)
+	r.PathPrefix("/").HandlerFunc(IndexHandler("frontend/dist/index.html"))
+	r.HandleFunc("/test", Gettest)
+	return r
+}
 
 func main() {
 	r := mux.NewRouter()
-	//r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("static"))))
-	r.HandleFunc("/temp", Static)
-	r.HandleFunc("/test", Gettest)
-	fmt.Println("starting server at 8 :8081")
-	log.Fatal(http.ListenAndServe(":8081", r))
+	// It's important that this is before your catch-all route ("/")
+	api := r.PathPrefix("/api/").Subrouter()
+	api.HandleFunc("/users", GetUsersHandler).Methods("GET")
+	// Serve static assets directly.
+	r.PathPrefix("/static").Handler(http.FileServer(http.Dir("frontend/dist")))
+	// Catch-all: Serve our JavaScript application's entry-point (index.html).
+	r.PathPrefix("/").HandlerFunc(IndexHandler("frontend/dist/index.html"))
+
+	srv := &http.Server{
+		Handler:      handlers.LoggingHandler(os.Stdout, r),
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+
 }
 
 //Gettest func..
 func Gettest(w http.ResponseWriter, r *http.Request) {
 	//vars := mux.Vars(r)
-	setupResponse(&w, r)
+	fmt.Println("Gettest Request")
+	//setupResponse(&w, r)
 	fmt.Fprintf(w, "first page2\n")
 }
 
@@ -32,4 +68,18 @@ func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+//GetUsersHandler function
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"id":        "123",
+		"timeStamp": time.Now().Format(time.RFC3339),
+	}
+	b, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	w.Write(b)
 }
